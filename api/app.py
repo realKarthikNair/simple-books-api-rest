e03237303d9e941332ca0349b09d757074bdbb52
Karthik Nair <realkarthiknair@gmail.com>
2024-04-19 13:18:26 +0530

    use real books

--- api/app.py
+++ api/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 books = [
-    {"title": "Book 1", "author": "Author 1", "publication_year": 2020},
-    {"title": "Book 2", "author": "Author 2", "publication_year": 2021},
-    {"title": "Book 3", "author": "Author 3", "publication_year": 2022},
+    {
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "publication_year": 1960,
+    },
+    {"title": "1984", "author": "George Orwell", "publication_year": 1949},
+    {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "publication_year": 1925,
+    },
+    {"title": "Pride and Prejudice", "author": "Jane Austen", "publication_year": 1813},
+    {
+        "title": "The Catcher in the Rye",
+        "author": "J.D. Salinger",
+        "publication_year": 1951,
+    },
 ]
 
 
